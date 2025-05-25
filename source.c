9cb2ec6e0b2a9e80cef49e88b3f8678ccbf5119c
pfostii <fabian.danek01@gmail.com>
2025-05-26 01:49:59 +0200

    Delete source.c

--- source.c
+++
@@ -1,9 +0,0 @@
-#include <string.h>
-
-int check_login(const char* user, const char* pass) {
-    if (strcmp(user, "admin") == 0 && strcmp(pass, "geheim") == 0) {
-        return 1; // Ja
-    } else {
-        return 0; // Kein Zugriff
-    }
-}
