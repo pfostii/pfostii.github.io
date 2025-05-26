af7113a1721d16677fe54c38f7a161c37fd69181
pfostii <fabian.danek01@gmail.com>
2025-05-26 03:16:05 +0200

    Delete source.c

--- source.c
+++
@@ -1,9 +0,0 @@
-#include <string.h>
-
-int check_login(const char* user, const char* pass) {
-    if (strcmp(user, "Philipp") == 0 && strcmp(pass, "Mikisek") == 0) {
-        return 1; // Ja
-    } else {
-        return 0; // Kein Zugriff
-    }
-}
